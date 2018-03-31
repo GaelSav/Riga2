7e3f0d996013408aa8721544bfc5c408fd167e1e
leprophete <gaelsavoure@hotmail.fr>
2018-03-31 20:59:27 +0200

    set GUI

--- src/ToolGui.java
+++ src/ToolGui.java
@@ -1,5 +1,40 @@
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class ToolGui {
 
-    
+    public static void addComponentsToPane(Container pane) {
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
+        addAButton("Must-seen", pane);
+
+    }
+
+    public static void addAButton(String text, Container container) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    public static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Riga");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
