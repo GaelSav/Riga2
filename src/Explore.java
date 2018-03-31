2511da9026965df0dda881709b1affdce9498fec
leprophete <gaelsavoure@hotmail.fr>
2018-03-31 21:11:55 +0200

    arrange frame

--- src/Explore.java
+++ src/Explore.java
@@ -2,13 +2,8 @@ public class Explore {
 
     public static void main(String[] args) {
 
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ToolGui.createAndShowGUI();
-            }
-        });
+        //show GUI
+        javax.swing.SwingUtilities.invokeLater(() -> ToolGui.createAndShowGUI());
 
     }
 
--- src/ToolGui.java
+++ src/ToolGui.java
@@ -1,5 +1,4 @@
-import java.awt.Component;
-import java.awt.Container;
+import java.awt.*;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,14 +18,10 @@ public class ToolGui {
         container.add(button);
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
     public static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("Riga");
+        frame.setPreferredSize(new Dimension(400, 150));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Set up the content pane.
