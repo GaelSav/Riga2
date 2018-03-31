38fe9d4f867b7f5f01d5385310abef0332b1b812
leprophete <gaelsavoure@hotmail.fr>
2018-03-31 21:51:12 +0200

    end of V1

--- src/ToolGui.java
+++ src/ToolGui.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,6 +8,7 @@ import javax.swing.JFrame;
 public class ToolGui {
 
     private JButton _mustSeenButton = new JButton("Must-seen");
+    private static String RIGA_MUST_SEEN = "Riga must seen.txt";
 
     public void addComponentsToPane(Container pane) {
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
@@ -23,7 +26,7 @@ public class ToolGui {
     public void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("Riga");
-        frame.setPreferredSize(new Dimension(400, 150));
+        frame.setPreferredSize(new Dimension(400, 90));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Set up the content pane.
@@ -35,7 +38,13 @@ public class ToolGui {
     }
 
     public void mustSeenButtonPressed() {
-        System.out.println("h");
+        File f = new File(RIGA_MUST_SEEN);
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.open(f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
